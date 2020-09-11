613435acc8598a23c9cbb9a8c024746a63427ba1
r38411 <r38411@yahoo.com>
2020-09-11 21:58:43 +0800

    assg1

--- Princeton-Alg2/Week1/wordnet/Outcast.java
+++ Princeton-Alg2/Week1/wordnet/Outcast.java
@@ -8,15 +8,43 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Outcast {
+    WordNet myWordNet;
 
     // constructor takes a WordNet object
     public Outcast(WordNet wordnet) {
-
+        myWordNet = wordnet;
     }
 
     // given an array of WordNet nouns,return an outcast
     public String outcast(String[] nouns) {
+        // build up the distance matrix
+        int nounLen = nouns.length;
+        int[][] dist = new int[nounLen][nounLen];
+        for (int i = 0; i < nouns.length; i++) {
+            for (int j = i + 1; j < nouns.length; j++)
+                dist[i][j] = myWordNet.distance(nouns[i], nouns[j]);
+        }
+        for (int i = 0; i < nounLen; i++)
+            dist[i][i] = 0;
+        for (int i = 1; i < nounLen; i++)
+            for (int j = 0; j < i; j++)
+                dist[i][j] = dist[j][i];
 
+        // build the d[] for each node
+        int[] d = new int[nounLen];
+        for (int i = 0; i < nounLen; i++) {
+            for (int j = 0; j < nounLen; j++)
+                d[i] += dist[i][j];
+        }
+        // find out the max index and value
+        int whichIndex = 0, whichDist = d[0];
+        for (int i = 1; i < nounLen; i++) {
+            if (d[i] > whichDist) {
+                whichIndex = i;
+                whichDist = d[i];
+            }
+        }
+        return nouns[whichIndex];
     }
 
     public static void main(String[] args) {
