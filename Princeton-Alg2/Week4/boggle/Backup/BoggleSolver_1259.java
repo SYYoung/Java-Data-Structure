d441bf3d885f7eb3f85f08285bf29e9f07a6e9f3
r38411 <r38411@yahoo.com>
2020-10-07 18:27:12 +0800

    assg5

---
+++ Princeton-Alg2/Week4/boggle/Backup/BoggleSolver_1259.java
@@ -0,0 +1,172 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+
+public class BoggleSolver {
+
+    private static final int MIN_WORD_LENGTH = 3;
+    private static final int UPPER_LENGTH_THRESHOLD = 8;
+    private static int[] points = { 1, 1, 2, 3, 5, 11 };
+    private ArrayList<String> wordsSoFar;
+    // the dictionary
+    private NewTrieSet dict;
+    private int leftOver;
+
+    private Bag<Integer>[] neighborList;
+
+    // Init the data structure using the given array of strings as the dictionary.
+    // You can assume each word in the dictionary contains only the upper letters A thru z
+    public BoggleSolver(String[] dictionary) {
+        wordsSoFar = new ArrayList<String>();
+        // 1. build the trie tree of the dictionary
+        dict = new NewTrieSet();
+        for (String s : dictionary) {
+            if (s.length() >= MIN_WORD_LENGTH)
+                dict.add(s);
+        }
+    }
+
+    // returns the set of all valid words in the given Boggle board, as an Iterable.
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        int rows = board.rows();
+        int cols = board.cols();
+        // set up the neighbor list
+        neighborList = (Bag<Integer>[]) new Bag[rows * cols];
+        for (int i = 0; i < rows * cols; i++)
+            neighborList[i] = new Bag<Integer>();
+        // neighborList = new ArrayList<Integer>[rows * cols] ();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                buildAdj(i, j, rows, cols, neighborList[i * cols + j]);
+            }
+        }
+        ArrayList<String> all = new ArrayList<String>();
+        // 1. enumerate all strings that can be composed by adj dice
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+                depthFirstSearch(board, i, j, all);
+        }
+
+        // 2. for each string, check the longest prefix
+        // 2. store the word
+        wordsSoFar = all;
+        return wordsSoFar;
+    }
+
+    // returns the score of the given word if it is in the dictionary, zero otherwise.
+    // you can assume the word contains only the uppercase letters A thru Z.
+    public int scoreOf(String word) {
+        // 1. check if the word is in dictionary. dict.contains(word) is true or not
+
+        // 2. calculate the score of the word
+        int score = calScore(word);
+        return score;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        String pre = "WAS";
+        StdOut.println("words which match: " + pre);
+        for (String s : solver.dict.keysWithPrefix(pre)) {
+            StdOut.println(s);
+        }
+        StdOut.println("The longest prefix of word: " + pre + " is : " +
+                               solver.dict.longestPrefixOf(pre));
+        BoggleBoard board = new BoggleBoard(args[1]);
+        StdOut.println(board);
+        int score = 0;
+
+        for (String word : solver.getAllValidWords(board)) {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);
+
+        String testStr = "CHLORDANE";
+        StdOut.println("Score of " + testStr + " : " + solver.scoreOf(testStr));
+
+    }
+
+    private void depthFirstSearch(BoggleBoard board, int row, int col, ArrayList<String> all) {
+        boolean[] marked = new boolean[board.rows() * board.cols()];
+        StringBuilder theWord = new StringBuilder();
+        dfs(board, row, col, theWord, all, marked);
+        // all.add(theWord.toString());
+        // reset the theWord and marked
+    }
+
+    private Bag<Integer> getAdj(int row, int col, int totalRow, int totalCol) {
+        return neighborList[row * totalCol + col];
+    }
+
+    private void buildAdj(int row, int col, int totalRow, int totalCol, Bag<Integer> neigh) {
+        for (int i = -1; i <= 1; i++)
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) continue;
+                int r = row + i;
+                int c = col + j;
+                if ((r >= 0) && (r < totalRow) &&
+                        ((c >= 0) && (c < totalCol)))
+                    neigh.add(r * totalCol + c);
+            }
+    }
+
+    // depth first search from v
+    private void dfs(BoggleBoard board, int row, int col, StringBuilder theWord,
+                     ArrayList<String> all,
+                     boolean[] marked) {
+        int totalRow = board.rows();
+        int totalCol = board.cols();
+
+        char ch = board.getLetter(row, col);
+        if (ch == 'Q')
+            theWord.append("QU");
+        else
+            theWord.append(ch);
+        if (!dict.isPrefixValid(theWord.toString())) {
+            theWord.deleteCharAt(theWord.length() - 1);
+            if (ch == 'Q')
+                theWord.deleteCharAt(theWord.length() - 1);
+            return;
+        }
+        marked[row * totalCol + col] = true;
+
+        // if such prefix exists in dictionary, if not, undo it
+        for (int w : getAdj(row, col, totalRow, totalCol)) {
+            if (!marked[w]) {
+                dfs(board, w / totalCol, w % totalCol, theWord, all, marked);
+            }
+
+        }
+        String s = theWord.toString();
+        if ((dict.contains(s)) && (!all.contains(s)))
+            all.add(theWord.toString());
+        // reset the theWord and marked
+        theWord.deleteCharAt(theWord.length() - 1);
+        if (ch == 'Q') // delete one more character
+            theWord.deleteCharAt(theWord.length() - 1);
+        marked[row * totalCol + col] = false;
+    }
+
+    private int calScore(String word) {
+        if (!dict.contains(word))
+            return 0;
+        if (word.length() < MIN_WORD_LENGTH)
+            return 0;
+        else if (word.length() >= UPPER_LENGTH_THRESHOLD)
+            return points[UPPER_LENGTH_THRESHOLD - MIN_WORD_LENGTH];
+        else
+            return points[word.length() - MIN_WORD_LENGTH];
+
+    }
+}
