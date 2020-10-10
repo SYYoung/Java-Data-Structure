901a66c7f375e09967cba4903175c3dcf66068ed
r38411 <r38411@yahoo.com>
2020-10-10 21:11:53 +0800

    assg5

--- Princeton-Alg2/Week5/burrows/BurrowsWheeler.java
+++ Princeton-Alg2/Week5/burrows/BurrowsWheeler.java
@@ -6,17 +6,19 @@
 
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdOut;
 
 import java.util.ArrayList;
 
 public class BurrowsWheeler {
+    private final static int R = 256;
 
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output
     public static void transform() {
         ArrayList<Character> lastColumn = new ArrayList<Character>();
         while (!BinaryStdIn.isEmpty()) {
-            int first = 0;
             String s = BinaryStdIn.readString();
             int len = s.length();
             CircularSuffixArray circularSufArr = new CircularSuffixArray(s);
@@ -38,15 +40,60 @@ public class BurrowsWheeler {
     // apply Burrows-WHeeler inverse transform,
     // reading from standard input and writing to standard output
     public static void inverseTransform() {
+        // read the index of first
+        int first;
+        /*
+        char[] t;
+        first = BinaryStdIn.readInt();
+        ArrayList<Character> inputT = new ArrayList<Character>();
         while (!BinaryStdIn.isEmpty()) {
-            String s = BinaryStdIn.readString();
+            char ch = BinaryStdIn.readChar();
+            inputT.add(ch);
         }
+        t = (char[])inputT.toArray();
+         */
+        first = 3;
+        String s = "ARD!RCAAAABB";
+        char[] t = s.toCharArray();
+
+        // sort the array t
+        int N = t.length;
+        Queue<Integer>[] tQueue = (Queue<Integer>[]) new Queue[R];
+        for (int i = 0; i < R; i++)
+            tQueue[i] = new Queue<Integer>();
+        int[] count1 = new int[R + 1];
+        int[] count2 = new int[R + 1];
+        char[] firstCol = new char[N];
+        int[] next = new int[N];
+        for (int i = 0; i < N; i++) {
+            count1[t[i] + 1]++;
+            tQueue[t[i]].enqueue(i);
+        }
+        count2[0] = count1[0];
+        for (int r = 0; r < R; r++) {
+            count1[r + 1] += count1[r];
+            count2[r + 1] = count1[r + 1];
+        }
+        for (int i = 0; i < N; i++)
+            firstCol[count2[t[i]]++] = t[i];
+
+        // build next array
+        for (int i = 0; i < N; i++) {
+            int pos = tQueue[firstCol[i]].dequeue();
+            next[i] = pos;
+        }
+
+        // for debugging
+        for (int i = 0; i < N; i++)
+            StdOut.println(firstCol[i]);
+        for (int i = 0; i < N; i++)
+            StdOut.println(next[i]);
+
     }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
     // if args[0] is "+", apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
-        BurrowsWheeler myBurrows = new BurrowsWheeler();
         if (args[0].equals("-"))
             BurrowsWheeler.transform();
         else
