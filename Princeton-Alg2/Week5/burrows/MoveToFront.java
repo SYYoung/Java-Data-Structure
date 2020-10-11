6204169e873501c5b397235ef7d2814defa4f5af
r38411 <r38411@yahoo.com>
2020-10-11 23:06:08 +0800

    assg5

--- Princeton-Alg2/Week5/burrows/MoveToFront.java
+++ Princeton-Alg2/Week5/burrows/MoveToFront.java
@@ -7,24 +7,44 @@
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
-import java.util.ArrayList;
 
 public class MoveToFront {
-    ArrayList<Character> charList;
     private static final int R = 256;
 
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
+        int[] indexList = new int[R];
         char[] charList = new char[R];
         // 0. init the sequence by making the ith character in the seq equal to the ith ASCII char
-        
-        // 1. read each 8-bit character c from standard input
-        while (!BinaryStdIn.isEmpty()) {
-            char ch = BinaryStdIn.readChar();
-            BinaryStdOut.write(charList[ch]);
+        for (int i = 0; i < R; i++) {
+            indexList[i] = i;
+            charList[i] = (char) i;
         }
+
+        // 1. read each 8-bit character c from standard input
         // 2. output 8-bit index in the seq where c appears
         // 3. move c to the front
+        // 4. update the index of c, and all char before c
+        String s = "ABRACADABRA!";
+        //for (int j = 0; j < s.length(); j++) {
+        //char ch = s.charAt(j);
+        while (!BinaryStdIn.isEmpty()) {
+            char ch = BinaryStdIn.readChar();
+            int pos = search(ch, charList);
+            BinaryStdOut.write(pos, 8);
+            System.arraycopy(charList, 0, charList, 1, pos);
+            charList[0] = ch;
+        }
+        BinaryStdOut.flush();
+        BinaryStdOut.close();
+    }
+
+    private static int search(char ch, char[] charList) {
+        for (int i = 0; i < charList.length; i++) {
+            if (charList[i] == ch)
+                return i;
+        }
+        return charList.length;
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
@@ -41,10 +61,9 @@ public class MoveToFront {
     // if args[0] is "-", apply move-to-front encoding
     // if args[0] is "+", apply move-to-front decoding
     public static void main(String[] args) {
-        MoveToFront myMove = new MoveToFront();
-        if (args[0] == "-")
-            myMove.encode();
+        if (args[0].equals("-"))
+            MoveToFront.encode();
         else
-            myMove.decode();
+            MoveToFront.decode();
     }
 }
